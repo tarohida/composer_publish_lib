9bfcdb0b447bb8d1a05a882ee394b1c1ef277119
vagrant <vagrant@homestead>
2020-10-20 06:02:49 +0000

    init

---
+++ tests/SampleClassTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace TaroHida\Sample\Tests; 
+
+use PHPUnit\Framework\TestCase;
+
+class SampleClassTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
